8e5db67a7da25754e57f2523ffc92e03a4ca4367
Osvaldo Cantone <65971428+ocantone@users.noreply.github.com>
2025-10-24 21:26:40 +0000

    Primer ejemplo en Codespace

---
+++ ejemplo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+
+    cout <<  endl << endl << "========================================";
+    cout <<  endl<< endl << "HOLA OSVALDO";
+    cout <<  endl << endl << "========================================";
+    cout << endl << endl;
+    return 0;
+}
